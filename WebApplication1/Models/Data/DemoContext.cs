0057b7f2361c3789efa59c9070ecf34ce43052ed
Mauri Demasi <demasimauri@gmail.com>
2022-10-01 19:19:46 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/Data/DemoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace DemoAPI.Models.Data
+{
+    public class DemoContext: DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,39 @@
+using DemoAPI.Models.Data;
+using DemoAPI.Models.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Get ConnectionString
+builder.Services.AddDbContext<DemoContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
+
+//Config Repository 
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
